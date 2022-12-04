d3e1a23dba34d0121f4325415e12cc16d5e1346f
Luis Felipe <202makertv@gmail.com>
2022-12-03 22:25:12 -0300

    Updated the node version and created a base structure in the project

---
+++ components/Base.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Dados e Props</h1>
+    <h2>Mutável: {{ texto }}</h2>
+    <h3>Vem aí uma propriedade: {{ another_text }}</h3>
+    <input v-model='texto'/>
+
+    <button @click="say(texto)">Clique em mim</button>
+    <button @click="testing()">Clique em mim 2</button>
+    <button @click="$emit('anotherFunction') && updateCounter()"> Emitir </button>
+
+    <h2>Counter: {{ counter }}</h2>
+
+  </div>
+</template>
+
+
+<style scoped>
+  /* SCOPED significa que os estilos aqui definidos serão aplicados apenas à esse componente */
+
+
+</style>
+
+<script>
+
+export default {
+  name: 'Base',
+  props: {
+    another_text: String
+  },
+  data() {
+    return {
+      texto: '',
+      counter: this.$store.getters.getCounter,
+    }
+  },
+  methods: {
+    say(something) { console.log(something) },
+
+    testing() {
+      console.log('Olá, mundo')
+    },
+
+    updateCounter() {
+      this.counter = this.$store.getters.getCounter
+    }
+
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Base another_text='Teste' @anotherFunction=handleAnotherFunction() />
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage',
+  methods: {
+    handleAnotherFunction() {
+      this.$store.commit('increment')
+      console.log("Função emitida!")
+    }
+  },
+}
+</script>
